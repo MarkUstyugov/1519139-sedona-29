b0c39dac1c4ac210ba70133eaadd9d57580c8000
Mark Ustyugov <68460553+MarkUstyugov@users.noreply.github.com>
2020-10-09 17:27:12 +0500

    Анимация выезда формы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var searchForm = document.querySelector('.form-container');
 var searchButton = document.querySelector('.search-button');
 
+
 searchButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   searchForm.classList.toggle('search-form-open');
