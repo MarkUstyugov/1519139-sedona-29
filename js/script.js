1015028d0dea184405605091d3de9b1eabbb4802
Mark Ustyugov <mark.ustyugov@gmail.com>
2020-10-12 20:50:05 +0500

    Форма поиска js + transform

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var searchForm = document.querySelector('.form-container');
 var searchButton = document.querySelector('.search-button');
 
+searchForm.classList.add('search-form-hidden');
 
-searchButton.addEventListener('click', function (evt) {
-  evt.preventDefault();
+searchButton.addEventListener('click', function () {
+  searchForm.classList.remove('search-form-hidden');
   searchForm.classList.add('search-form-open');
 });
